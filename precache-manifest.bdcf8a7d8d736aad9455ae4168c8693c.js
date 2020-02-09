f43a7e56bdac0b8c395849f450c7831febdc138b
unknown <arshia_nazarlotfi1376@yahoo.com>
2020-02-10 03:05:05 +0330

    Updates

---
+++ precache-manifest.bdcf8a7d8d736aad9455ae4168c8693c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dc465a36b60b66c84504f06e9a6a65",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f54a88bc1a92a261c5ec",
+    "url": "/monsters-rolodex/static/css/main.a0c578fe.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54a88bc1a92a261c5ec",
+    "url": "/monsters-rolodex/static/js/main.312ce4c2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
